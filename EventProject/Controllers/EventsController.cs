2e9f283071b3c20e7d5fcba1d88c7e14586701c8
Tiller <ctiller@proforma.com>
2018-04-26 15:08:21 -0400

    Create generic GET request for all events

---
+++ EventProject/Controllers/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using EventProject.Data;
+using EventProject.Models;
+using EventProject.ViewModels.Events;
+
+namespace EventProject.Controllers
+{
+    public class EventsController : ApiController
+    {
+        // GET: Find all events in the city.
+        [Route("api/events")]
+        [HttpGet]
+        public IEnumerable<Event> GetEvents([FromUri]GetEvent Event)
+        {
+            using (var db = new EventContext())
+            {
+                var query = db.Events.Include(i => i.City).OrderBy(o => o.Title).ToList();
+                return query;
+            }
+        }
+    }
+}
---
+++ EventProject/ViewModels/Events/GetEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventProject.ViewModels.Events
+{
+    public class GetEvent
+    {
+        public string Title { get; set; }
+        public string TagLine { get; set; }
+        public string Long_Description { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public int? Age_Limit { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateHappening { get; set; }
+    }
+}
