28458978c8f765f46869af9bf30f88193c7b5a17
Tiller <ctiller@proforma.com>
2018-04-26 13:27:16 -0400

    install entityframework, create dbcontext, set up connectionstrings in web config

---
+++ EventProject/DbContext/EventContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EventProject.Data
+{
+    public class EventContext :DbContext
+    {
+        public EventContext() : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
