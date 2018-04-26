270c182acd61588ae50f98c5e43bb84e4d475656
Tiller <ctiller@proforma.com>
2018-04-26 13:32:48 -0400

    Create POCOs w/o connections

---
+++ EventProject/Models/Attendee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventProject.Models
+{
+    public class Attendee
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ EventProject/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventProject.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Tagline { get; set; }
+        public string Long_Description { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int? Zip { get; set; }
+        public int? Age_Limit { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime DateHappening { get; set; }
+    }
+}
