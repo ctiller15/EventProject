608b9acaa2740974bb7e43af53fbd7bc5e192692
Tiller <ctiller@proforma.com>
2018-04-28 21:52:43 -0400

    User can now search for an event by title

--- EventProject/Scripts/app.js
+++ EventProject/Scripts/app.js
@@ -3,24 +3,35 @@
 const app = angular.module("main", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
-    // events.
+    // events. Index route.
     $routeProvider.when("/events", {
         templateUrl: "/Scripts/app/views/events.html",
         controller: "allEventsController"
     })
 
     $routeProvider.otherwise({ redirectTo: "/events" });
-})
+});
 
 app.controller("allEventsController", ["$scope", "$http", function ($scope, $http) {
-    $scope.message = "Hello!!!";
-    console.log("Hello!");
+    $scope.searchParams = {
+        Title: "",
+    }
 
-    $http({
-        method: "GET",
-        url: "/api/events"
-    }).then(res => {
-        $scope.events = res.data;
-        console.log($scope.events);
-    })
-}])
+    $scope.searchForEvents = () => {
+        console.log($scope.searchParams);
+        let url = "/api/events";
+        if ($scope.searchParams.Title) {
+            url += `?Title=${$scope.searchParams.Title}`;
+        }
+
+        $http({
+            method: "GET",
+            url: url,
+        }).then(res => {
+            $scope.events = res.data;
+            console.log($scope.events);
+        });
+    }
+
+    $scope.searchForEvents();
+}]);
